751038d7420067398a053eada87fbaa7b32bcbac
Yuliia-03 <yuliia.bohak03@gmail.com>
2026-02-27 02:48:43 +0000

    Update samples

--- frontend/src/sample_graphs/sample_graphs.js
+++ frontend/src/sample_graphs/sample_graphs.js
@@ -37,7 +37,7 @@ export const Samples = {
             { data: { id: "1-2", source: "1", target: "2", weight: 4 } },
             { data: { id: "1-3", source: "1", target: "3", weight: 3 } },
             { data: { id: "2-4", source: "2", target: "4", weight: 2 } },
-            { data: { id: "3-4", source: "4", target: "3", weight: 4 } },
+            { data: { id: "4-3", source: "4", target: "3", weight: 4 } },
             { data: { id: "1-4", source: "1", target: "4", weight: 5 } },
             
             { data: { id: "4-5", source: "4", target: "5", weight: 1 } },
@@ -67,7 +67,7 @@ export const Samples = {
             { data: { id: "2-4", source: "2", target: "4", weight: 1 } },
             { data: { id: "4-5", source: "4", target: "5", weight: 1 } },
             
-            { data: { id: "2-5", source: "5", target: "2", weight: 1 } },
+            { data: { id: "5-2", source: "5", target: "2", weight: 1 } },
             { data: { id: "3-6", source: "3", target: "6", weight: 1 } },
             { data: { id: "4-6", source: "4", target: "6", weight: 1 } },
             { data: { id: "6-7", source: "6", target: "7", weight: 1 } },
@@ -119,27 +119,27 @@ export const Samples = {
         edges: [
             { data: { id: "2-3", source: "2", target: "3", weight: 4 } },
             { data: { id: "3-4", source: "3", target: "4", weight: 3 } },
-            { data: { id: "2-4", source: "4", target: "2", weight: 2 } },
+            { data: { id: "4-2", source: "4", target: "2", weight: 2 } },
 
             { data: { id: "4-9", source: "4", target: "9", weight: 4 } },
             { data: { id: "9-4", source: "9", target: "4", weight: 5 } },
             
-            { data: { id: "4-5", source: "5", target: "4", weight: 1 } },
-            { data: { id: "2-5", source: "5", target: "2", weight: 1 } },
+            { data: { id: "5-4", source: "5", target: "4", weight: 1 } },
+            { data: { id: "5-2", source: "5", target: "2", weight: 1 } },
 
             { data: { id: "5-6", source: "5", target: "6", weight: 6 } },
             { data: { id: "6-5", source: "6", target: "5", weight: 8 } },
-            { data: { id: "5-7", source: "7", target: "5", weight: 2 } },
-            { data: { id: "6-8", source: "8", target: "6", weight: 3 } },
+            { data: { id: "7-5", source: "7", target: "5", weight: 2 } },
+            { data: { id: "8-6", source: "8", target: "6", weight: 3 } },
 
             { data: { id: "7-8", source: "7", target: "8", weight: 6 } },
 
-            { data: { id: "7-10", source: "10", target: "7", weight: 2 } },
+            { data: { id: "10-7", source: "10", target: "7", weight: 2 } },
             { data: { id: "8-10", source: "8", target: "10", weight: 2 } },
 
-            { data: { id: "9-10", source: "10", target: "9", weight: 2 } },
+            { data: { id: "10-9", source: "10", target: "9", weight: 2 } },
             { data: { id: "10-11", source: "10", target: "11", weight: 2 } },
-            { data: { id: "3-11", source: "11", target: "3", weight: 2 } },
+            { data: { id: "11-3", source: "11", target: "3", weight: 2 } },
         ],
     },
 
