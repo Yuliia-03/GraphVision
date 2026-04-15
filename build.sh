8d8f4be70747eb1dcc4da32749458e708d190564
Yuliia-03 <yuliia.bohak03@gmail.com>
2026-04-15 15:35:26 +0100

    Deployment files

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ cd backend
 pip install -r requirements.txt
 python manage.py makemigrations api
 python manage.py migrate
-python manage.py seed_data
+python manage.py add_samples
 python manage.py collectstatic --noinput
