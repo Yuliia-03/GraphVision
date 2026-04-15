d1664d4ba8fcd806b4cebc4a02bf4bd577590127
Yuliia Bohak <61803249+Yuliia-03@users.noreply.github.com>
2026-04-15 18:27:46 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,10 +96,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default="sqlite:///db.sqlite3",
-        conn_max_age=600
-    )
+    "default": dj_database_url.parse(os.environ["DATABASE_URL"])
 }
 
 
