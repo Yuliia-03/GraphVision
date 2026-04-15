97b1cecc17823032ed3e80825b1171354da38ca5
Yuliia Bohak <61803249+Yuliia-03@users.noreply.github.com>
2026-04-15 18:04:13 +0100

    Update DAG_Controls.jsx

--- frontend/src/components/controlsPanel/subtaskControls/DAG_Controls.jsx
+++ frontend/src/components/controlsPanel/subtaskControls/DAG_Controls.jsx
@@ -7,7 +7,7 @@ export default function DAGControls({mode = "explore" }) {
     return (
         <div className="algo-controls">
             <div className="config-panel">
-                <h3>DAG</h3>
+                <h3 className="config-title">Directed Acyclic Graph Checking</h3>
             </div>
 
             <ButtonPanel mode={mode} />
